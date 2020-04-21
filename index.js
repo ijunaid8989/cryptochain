364764d9ccc1ccc442e7f902fe209df73d20f5e2
Junaid Farooq <junaid@evercam.io>
2020-04-21 11:49:05 +0500

    Added redis and PubSub module.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express")
 const Blockchain = require("./blockchain")
+const bodyParser = require("body-parser")
+
+const PubSub = require("./pubsub")
 
 const app = express()
 const blockchain = new Blockchain()
+const pubsub = new PubSub({ blockchain })
+
+setTimeout(() => pubsub.broadcastChain(), 1000)
+
+app.use(bodyParser.json())
 
 app.get("/api/blocks", (req, res) => {
   res.json(blockchain.chain)
+})
+
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body
+
+  blockchain.addBlock({ data })
+
+  res.redirect("/api/blocks")
 })
 
 const PORT = 3000
